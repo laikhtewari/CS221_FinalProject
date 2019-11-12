c1f02ac345809d57aa5243a0fec344bf4a2fddc5
Stephen Ren <stephrenny23@gmail.coml>
2019-11-11 22:28:54 -0800

    Added some skeleton for new states

--- model.py
+++ model.py
@@ -1,6 +1,36 @@
 import util, math, random
 from collections import defaultdict
 
+# Maybe we can have some different way to keep track of all of this and
+# Create a parent class for each of these successor states
+
+class DisasterStatus():
+    def __init__(self):
+        """
+
+        """
+
+class Personel():
+    def __init__(self):
+        """
+        Keeps track of the staff we have available at a given time, with their own specializations
+        and how they might be able to assist in specific scenariors
+        """
+
+class Resources():
+    def __init__(self, personel, money=0, expected_income=0):
+        """
+        Takes in as input different state
+        """
+        self.money = money
+        self.expected_income = expected_income
+
+    def get_money(self):
+        return self.money
+
+def combine_states(state1, state2):
+
+
 class DisasterMDP(util.MDP):
     def __init__(self, resources, disaster_status):
         """
